f304e7ee85bfee209318157f052ecdf74fbd7626
kimjin <hjklkj12@gmail.com>
2023-01-07 15:32:09 +0900

    bottom navigation view

--- jining_app/app/src/main/java/com/example/jin_ing/MainActivity.kt
+++ jining_app/app/src/main/java/com/example/jin_ing/MainActivity.kt
@@ -2,73 +2,35 @@ package com.example.jin_ing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-import android.widget.LinearLayout
-import android.R
-import android.view.MenuItem
-
+import androidx.fragment.app.Fragment
 import com.example.jin_ing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // 전역 변수로 바인딩 객체 선언
     private var mBinding: ActivityMainBinding? = null
-    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = mBinding!!
 
-    var home_ly: LinearLayout? = null
-    var bottomNavigationView: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        home_ly = binding.homeLy
-        bottomNavigationView = binding.bottomNavigationView
+        supportFragmentManager.beginTransaction().add(binding.container.id, HomeFragment()).commit()
 
-        //맨 처음 시작할 탭 설정
-        bottomNavigationView!!.selectedItemId = R.id.home
-
-        bottomNavigationView!!.setOnItemSelectedListener { item ->
-            changeFragment(
-                when (item.itemId) {
-                    R.id.home -> {
-                        bnv_main.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                        bnv_main.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                        NaviHomeFragment()
-                        // Respond to navigation item 1 click
-                    }
-                    R.id.pro -> {
-                        bnv_main.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                        bnv_main.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                        NaviAttendanceFragment()
-                        // Respond to navigation item 2 click
-                    }
-                    R.id.first3 -> {
-                        bnv_main.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                        bnv_main.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv2)
-                        NaviInfoFragment()
-                        // Respond to navigation item 3 click
-                    }
-                    else -> {
-                        bnv_main.itemIconTintList = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                        bnv_main.itemTextColor = ContextCompat.getColorStateList(this, R.color.color_bnv1)
-                        NaviCertificateFragment()
-                    }
+        binding.bn.setOnNavigationItemSelectedListener {
+            replaceFragment(
+                when (it.itemId) {
+                    R.id.home-> HomeFragment()
+                    R.id.rank-> RankFragment()
+                    else -> ProfileFragment()
                 }
             )
             true
         }
-        bnv_main.selectedItemId = R.id.first
     }
 
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fl_con, fragment)
-            .commit()
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.container.id, fragment).commit()
     }
-
 }
