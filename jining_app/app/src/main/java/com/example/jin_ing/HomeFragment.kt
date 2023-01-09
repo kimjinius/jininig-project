16f35ff600376c06fc01e31228ddef2a20790407
kimjin <hjklkj12@gmail.com>
2023-01-09 23:02:01 +0900

    naver maps

--- jining_app/app/src/main/java/com/example/jin_ing/HomeFragment.kt
+++ jining_app/app/src/main/java/com/example/jin_ing/HomeFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.annotation.Nullable
+import com.naver.maps.map.MapView
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var mapView: MapView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup,
-                              savedInstanceState: Bundle): View {
-        // ...
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView = view.findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
